cdac0a5ab31bd451ce3c86d90448ee0d34ba6888
Olena <inki89761@gmail.com>
2022-10-22 12:03:44 +0300

    instaled bootstrap into react-app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <div className="container">
+        <header className="App-header">hello</header>
+
+        <button type="button" className="btn btn-primary">
+          Search
+        </button>
+      </div>
     </div>
   );
 }
