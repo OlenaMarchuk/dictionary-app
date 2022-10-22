46fcf09ad2006cc55efac92793756080b2d34f67
Olena <inki89761@gmail.com>
2022-10-22 13:44:40 +0300

    added header to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import logo from "./logo-book.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">hello</header>
+        <header className="App-header mt-3 clearfix">
+          <h1>
+            <img src={logo} className="img-fluid " alt="logo" />{" "}
+            <span>DICTIONARY</span>
+          </h1>
+          <h4>Make your words meaningful</h4>
+        </header>
 
         <button type="button" className="btn btn-primary">
           Search
@@ -14,5 +21,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
