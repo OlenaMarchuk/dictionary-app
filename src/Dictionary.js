1e9c49b74f1f77bbf60110494dce72b89aed28ef
Olena <inki89761@gmail.com>
2022-11-12 21:13:32 +0200

    added photos to the dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultedKeyword);
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
-  function getResponse(response) {
+  const [photos, setPhotos] = useState(null);
+  function getPexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+  function getDictResponse(response) {
     setResults(response.data[0]);
   }
   function search() {
     //documentation: https//dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(getResponse);
+    axios.get(apiUrl).then(getDictResponse);
+    let apiPexelsKey = `563492ad6f9170000100000151f312400a614c77be93fa57afaa369c`;
+    let apiPexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${apiPexelsKey}` };
+    axios.get(apiPexelsUrl, { headers: headers }).then(getPexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -29,7 +38,7 @@ export default function Dictionary(props) {
   }
   if (loaded) {
     return (
-      <div className="Dictionary ">
+      <div className="Dictionary">
         <section>
           <h4 className="question">What word would you like to look up?</h4>
           <form className="input-group" onSubmit={handleSubmit}>
@@ -45,6 +54,7 @@ export default function Dictionary(props) {
           <div className="hint">i.e. forest, sunset, phone...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row mt-3">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank">
+                  <img
+                    className="img-fluid rounded mb-3"
+                    src={photo.src.landscape}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
